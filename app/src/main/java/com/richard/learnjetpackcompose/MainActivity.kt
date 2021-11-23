dcf6d375f0eb26dc4db056557300580ccd4fdec6
ThankGod Richard <madueketf@gmail.com>
2021-11-23 04:08:58 +0100

    Added simple photocard

--- app/src/main/java/com/richard/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/richard/learnjetpackcompose/MainActivity.kt
@@ -1,32 +1,27 @@
 package com.richard.learnjetpackcompose
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import com.richard.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,110 +36,53 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
-private fun Greeting(name: String) {
-    Card(
-        backgroundColor = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        CardContent(name)
-    }
+fun myApp(){
+
 }
 
-@Composable
-private fun CardContent(name: String) {
-    var expanded by remember { mutableStateOf(false) }
 
-    Row(
-        modifier = Modifier
-            .padding(12.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                    stiffness = Spring.StiffnessLow
-                )
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable { /* Ignoring onClick */ }
+        .padding(16.dp)) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "Contact profile picture",
             )
-    ) {
+        }
         Column(
             modifier = Modifier
-                .weight(1f)
-                .padding(12.dp)
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
         ) {
-            Text(text = "Hello, ")
-            Text(
-                text = name,
-                style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                )
-            )
-            if (expanded) {
-                Text(
-                    text = ("Composem ipsum color sit lazy, " +
-                            "padding theme elit, sed do bouncy. ").repeat(4),
-                )
-            }
-        }
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(
-                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                contentDescription = if (expanded) {
-                    stringResource(R.string.show_less)
-                } else {
-                    stringResource(R.string.show_more)
-                }
-
-            )
-        }
-    }
-}
-
-@Composable
-fun myApp() {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(false) }
-
-    if (shouldShowOnboarding) {
-        OnBoardongScreen(onContinueClicked = { shouldShowOnboarding = false })
-    } else {
-        Greetings()
-    }
-}
-
-@Composable
-fun Greetings(names: List<String> = List(1000) { "$it" }){
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names){ name ->
-            Greeting(name = name)
-        }
-    }
-}
-
-
-
-@Composable
-fun OnBoardongScreen(onContinueClicked: () -> Unit){
-    androidx.compose.material.Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to home screen")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick =  onContinueClicked) {
-                    Text(text = "Continue")
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
             }
         }
     }
 }
 
-
-@Preview(showBackground = true,
-    uiMode = UI_MODE_NIGHT_YES,
-    widthDp = 320)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun PhotographerCardPreview() {
     LearnJetpackComposeTheme {
-       myApp()
+        PhotographerCard()
     }
 }
+
+
+
